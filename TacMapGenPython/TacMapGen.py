c39029b447a2611538d2f45f6ce312e70032cbfd
TheChemistAstronaut <andriyyatsykiv+extra@gmail.com>
2023-09-15 14:17:48 -0400

    Added Jupyter notebook

--- TacMapGenPython/TacMapGen.py
+++ TacMapGenPython/TacMapGen.py
@@ -4,53 +4,76 @@ import numpy as np
 import geopandas
 import pandas
 
-#IMPORT BOUNDING BOX
+# USER SETTINGS
+    #Asks user for color or black and white
+colorindex = int(input("Color or Black and White? Input 0 (Zero) for color or 1 for black and white:\n"))
+    #Asks user for number of grid marks
+gridlines = int(input("How many grids do you want to the map to be divided into?\n"))
+
+# SET POINTS OF INTEREST
+#Retrieve named points
+#Plot named points as dots, with labels according
+
+# IMPORT BOUNDING BOX
 north = 41.466901
 south = 41.462273
 east = -74.09598
 west = -74.102378
 
-#COLORS
-    #set 0 for color and 1 for BW
-colorindex = 1
-backgroundcolor = ['#7AC676', '#b7b7b7']
+# COLOR LIBRARY
+    # set 0 for color and 1 for BW
+forestcolor = ['#7AC676', '#EAEAEA']
 roadcolor = ['#644117', '#7f7f7f']
 grasscolor = ['#BBEAB8', '#EAEAEA']
 buildingcolor = ['#696969', '#939393']
 watercolor = ['#aad3df', '#dddbdb']
 
-#TAG LIST
+# TAG LIST
 buildingtags = {'building': True}
-grasstags = {'landuse': ['grass','recreation_ground']}
+grasstags = {'landuse': ['grass', 'recreation_ground']}
 roadtags = {'highway': ['track', 'footway', 'service'], 'amenity': 'parking'}
 watertags = {'natural': 'water'}
+foresttags = {'natural': 'wood'}
+nametags = {'name': True}
 
-#IMPORT GEODATA
+# IMPORT GEODATA
+forest = osm.features_from_bbox(north, south, east, west, foresttags)
 grass = osm.features_from_bbox(north, south, east, west, grasstags)
 water = osm.features_from_bbox(north, south, east, west, watertags)
 roads = osm.features_from_bbox(north, south, east, west, roadtags)
 buildings = osm.features_from_bbox(north, south, east, west, buildingtags)
+names = osm.features_from_bbox(north, south, east, west, nametags)
 
-#CREATE PLOTS IN LAYERS
-grassplot = grass.plot(color=grasscolor[colorindex])
+# CREATE PLOTS IN LAYERS
+    # plots hatched forest for black and white
+if colorindex == 1:
+    forestplot = forest.plot(color=forestcolor[colorindex], edgecolor="black")
+else:
+    forestplot = forest.plot(color=forestcolor[colorindex])
+
+    # plots border around grass if black and white
+if colorindex == 1:
+    grassplot = grass.plot(ax=forestplot, color=grasscolor[colorindex], edgecolor="black")
+else:
+    grassplot = grass.plot(ax=forestplot, color=grasscolor[colorindex])
+
 waterplot = water.plot(ax=grassplot, color=watercolor[colorindex])
 roadsplot = roads.plot(ax=waterplot, color=roadcolor[colorindex])
-buildings = buildings.plot(ax=roadsplot, marker='o', color=buildingcolor[colorindex], markersize=5)
+buildingplot = buildings.plot(ax=roadsplot, marker='o', color=buildingcolor[colorindex], markersize=5)
 
-finalplot = buildings
+finalplot = buildingplot
 
-#TICK CALCULATIONS
-    #How many gridlines
-gridlines = 20
+# TICK CALCULATIONS
 
-#NUMBERED GRIDLINES
+    # Calculates the division between ticks
 ytickdiv = (north-south)/gridlines
 xtickdiv = (east-west)/gridlines
 
+    # Calculates 0.5 tick measurement for the minor tick offset
 half_y_tickdiv = ytickdiv/2
 half_x_tickdiv = xtickdiv/2
 
-#Sets the MAJOR ticks (for grid)
+    # Sets the MAJOR ticks (for grid)
 yticklist = []
 xticklist = []
 
@@ -58,7 +81,7 @@ for number in list(range(gridlines)):
     yticklist.append(south+ytickdiv*number)
     xticklist.append(west+xtickdiv*number)
 
-#Sets the MINOR ticks (for labels)
+    # Sets the MINOR ticks (for labels)
 yticklist_minor = []
 xticklist_minor = []
 
@@ -66,28 +89,27 @@ for number in list(range(gridlines)):
     yticklist_minor.append(south+half_y_tickdiv+ytickdiv*number)
     xticklist_minor.append(west+half_x_tickdiv+xtickdiv*number)
 
-#LETTER GRIDLINES - Generates list of capital letters to use for letter labeling
+    # LETTER GRIDLINES - Generates list of capital letters to use for letter labeling
 lastletter = 97 + gridlines
 xticklabels = list(map(chr, range(97, lastletter)))
 xticklabels = list(map(str.upper, xticklabels))
 
-#NUMBERED GRIDLINES - Generates list of numbers for use with number labels
+    # NUMBERED GRIDLINES - Generates list of numbers for use with number labels
 yticklabels = list(range(gridlines))
 
-
-#Creates the MINOR ticks for labeling
+    # Creates the MINOR ticks for labeling
 plt.yticks(yticklist_minor, yticklabels, minor=True)
 plt.xticks(xticklist_minor, xticklabels, minor=True)
 
-#Creates the MAJOR ticks for the grid
-#Sets a blank list because otherwise labels are auto-written, even if labels=None
+    # Creates the MAJOR ticks for the grid
+    # Sets a blank list because otherwise labels are auto-written, even if labels=None
 blank = []
 plt.yticks(yticklist, blank)
 plt.xticks(xticklist, blank)
 
 
-#CUSTOMIZATION
-finalplot.set_facecolor((backgroundcolor[colorindex]))
+# PLOT INITIALIZATION
+# finalplot.set_facecolor((backgroundcolor[colorindex]))
 plt.grid(which='major', linestyle='-', lw=0.5, color='grey')
 plt.xlim(west, east)
 plt.ylim(south, north)
